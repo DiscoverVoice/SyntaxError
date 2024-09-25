6ecee80836690822d93e5248b5dad0d22492240d
meatcarrot <ninguis555@gmail.com>
2024-09-25 21:08:49 +0900

    Feat: 1. Added Announcer directory at src 2. Build Vscode Extension 3. Need to change for npm install in EX Folder

--- src/EX/Prototype/extension.js
+++ src/EX/Prototype/extension.js
@@ -12,16 +12,16 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "Announcer" is now active!');
+	console.log('Congratulations, your extension "Prototype" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('Announcer.helloWorld', function () {
+	const disposable = vscode.commands.registerCommand('Prototype.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from !');
+		vscode.window.showInformationMessage('Hello World from Announcer!');
 	});
 
 	context.subscriptions.push(disposable);
