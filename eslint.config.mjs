150ffe19c1d818bef92695117d0d2b346cb69068
hemaher0 <hemahero@naver.com>
2024-10-01 23:33:16 +0900

    Refactor: Integrated modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,25 +5,38 @@ import prettierPlugin from "eslint-plugin-prettier";
 import airbnbConfig from "eslint-config-airbnb";
 import tsParser from "@typescript-eslint/parser";
 
-export default [
-  {
-    files: ["src/**/*.{js,mjs,cjs,ts,tsx}"],
+export default [{
+    files: ["**/*.js"],
     languageOptions: {
-      parser: tsParser,
-      globals: { ...globals.node, process: "readonly" }
+        parser: tsParser,
+        globals: {
+            ...globals.commonjs,
+            ...globals.node,
+            ...globals.mocha,
+            ...globals.node, process: "readonly"
+        },
+
+        ecmaVersion: 2022,
+        sourceType: "module",
     },
     plugins: {
-      prettier: prettierPlugin,
-      "@typescript-eslint": tseslint
+        prettier: prettierPlugin,
+        "@typescript-eslint": tseslint
     },
     rules: {
-      ...pluginJs.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
-      ...airbnbConfig.rules,
-      "prettier/prettier": ["error", { semi: true }],
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/explicit-function-return-type": "warn",
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }]
-    }
-  }
-];
+        ...pluginJs.configs.recommended.rules,
+        ...tseslint.configs.recommended.rules,
+        ...airbnbConfig.rules,
+        "prettier/prettier": ["error", { semi: true }],
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/explicit-function-return-type": "warn",
+        "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+        "no-const-assign": "warn",
+        "no-this-before-super": "warn",
+        "no-undef": "warn",
+        "no-unreachable": "warn",
+        "no-unused-vars": "warn",
+        "constructor-super": "warn",
+        "valid-typeof": "warn",
+    },
+}];
