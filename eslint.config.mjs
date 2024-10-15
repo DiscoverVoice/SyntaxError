17b4e0a0aaec1eae751c43dc8e4574e21fe6d0c9
hemaher0 <hemahero@naver.com>
2024-10-15 22:50:52 +0900

    feat: add linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,28 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import prettierPlugin from "eslint-plugin-prettier";
+import airbnbConfig from "eslint-config-airbnb";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    files: ["*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.mocha
+      },
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...airbnbConfig.rules,
+      "prettier/prettier": "error",
+    }
+  },
 ];
