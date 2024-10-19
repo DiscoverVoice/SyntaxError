720ad0d61737aba6185d9c1eedc434cfb5ef4949
meatcarrot <ninguis555@gmail.com>
2024-10-19 17:20:59 +0900

    Fixed : folder and naming function

--- src/temp.cpp
+++ src/temp.cpp
@@ -1,67 +1,36 @@
-#include<iostream>
+#include <iostream>
+#include <set>
+#include <vector>
+
 using namespace std;
 
-void merge(int arr[], int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-
-    int L[n1], R[n2];
-
-    for (int i = 0; i < n1; ++i)
-        L[i] = arr[l + i];
-    for (int j = 0; j < n2; ++j)
-        R[j] = arr[m + 1 + j];
-
-    int i = 0, j = 0;
-
-    int k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        }
-        else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void mergeSort(int arr[], int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-
-        merge(arr, l, m, r);
-    }
-}
-
-void printArray(int A[], int size) {
-    for (int i = 0; i < size; i++)
-        cout << A[i] << " ";
-    cout << endl;
-}
-
 int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    mergeSort(arr, 0, n - 1);
-    cout << "Sorted array: \n";
-    printArray(arr, n);
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    set<int> blue_cards;
+    
+    for (int i = 0; i < M; ++i) {
+        int card;
+        cin >> card;
+        blue_cards.insert(card);
+    }
+
+    vector<int> chulsoo_cards(K);
+    for (int i = 0; i < K; ++i) {
+        cin >> chulsoo_cards[i];
+    }
+
+    for (int i = 0; i < K; ++i) {
+        int chulsoo_card = chulsoo_cards[i];
+        
+        auto it = blue_cards.upper_bound(chulsoo_card);
+        
+        if (it != blue_cards.end()) {
+            cout << *it << endl;
+            blue_cards.erase(it);
+        }
+    }
+
     return 0;
 }
